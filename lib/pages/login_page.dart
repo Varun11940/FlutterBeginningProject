d660cba2da07f732af0f9cf7db91b0eb1324d86d
varun2427 <60778318+Varun11940@users.noreply.github.com>
2022-01-13 23:40:29 +0530

    day5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,57 +5,59 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/login_image.png",
-              fit: BoxFit.cover,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Text(
-              "Welcome",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/login_image.png",
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      // hintText: " Enter UserName",
-                      labelText: "Enter Username",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      // hintText: " Enter UserName",
-                      labelText: "Enter Password",
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    child: Text("login"),
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      print("Thanks for Submission");
-                    },
-                  )
-                ],
+              SizedBox(
+                height: 20.0,
               ),
-            )
-          ],
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        // hintText: " Enter UserName",
+                        labelText: "Enter Username",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        // hintText: " Enter UserName",
+                        labelText: "Enter Password",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      child: Text("login"),
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        print("Thanks for Submission");
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
